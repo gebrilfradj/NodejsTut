b3e2dc781ce23db9ed91074ff89e9ab8348d0821
gebrilfradj <gfradj@ufl.edu>
2023-11-19 00:55:31 -0500

    lesson 1 done

---
+++ server.js
@@ -0,0 +1,11 @@
+console.log('Hello World')
+
+const os = require('os')
+const path = require('path')
+
+const {add, subtract, multiply, divide} = require('./math')
+
+console.log(add(2,3))
+console.log(subtract(2,3))
+console.log(multiply(2,3))
+console.log(divide(2,3))
