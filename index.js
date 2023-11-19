37df400280acd872695cc62e61ea5cf55c94b8d8
gebrilfradj <gfradj@ufl.edu>
2023-11-19 03:36:02 -0500

    lesson 3 done

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const fsPromises = require('fs').promises;
-const path = require('path');
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
 
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'))
 
-const fileOps = async() => {
-  try{
-    const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
-    console.log(data);
-    await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'));
-    await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '/n/nNice to meet you');
-    await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data);
-    await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'), path.join(__dirname, 'files', 'promiseComplete.txt'));
-    const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt'), 'utf8');
-    console.log(newData);
-  } catch(err){
-    console.log(err);
-  }
-}
+console.log(uuid())
+
+console.log()
--- stream.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const rs = fs.createReadStream('./files/lorem.txt', {encoding: 'utf8'});
-
-const ws = fs.createWriteStream('./files/lorem.txt');
-
-rs.pipe(ws);
